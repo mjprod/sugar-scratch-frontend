109e001d2883b0f0a43674e683d4f991514543b4
leoblkwht <84880425+leoblkwht@users.noreply.github.com>
2026-09-09 16:14:28 +1000

    fix for typescript issue

--- src/features/swipe/components/SwipeCard.tsx
+++ src/features/swipe/components/SwipeCard.tsx
@@ -193,7 +193,6 @@ export const SwipeCard = memo(function SwipeCard({
             poster={posterUrl || undefined}
             className={ready ? 'is-ready' : undefined}
             muted
-            defaultMuted
             loop
             playsInline
             autoPlay={playing}
